26c1bb57c0b272496da688a7d4e08f9b975a0ea6
juandinella <juan.dinella@gmail.com>
2014-07-07 19:33:09 -0300

    empecé housing

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ $(document).ready(function() {
 
 
 });
+
+$(function() {
+    $("#slider-horizontal").slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 1,
+        max: 50,
+        value: 1,
+        slide: function(event, ui) {
+            $("#amount").val(ui.value);
+        }
+    });
+    $("#amount").val($("#slider-horizontal").slider("value"));
+});
