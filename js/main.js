2e037b5b29e5e97c7c5797ff929b3e1da2a7c6eb
juandinella <juan.dinella@gmail.com>
2014-07-10 17:45:55 -0300

    empece con el responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var collapsed = true;
+var collapsed_mobile = true;
+
+
+
 $(document).ready(function() {
     $('.country').hover(function() {
         $(".othersCountries").slideDown("fast");
@@ -21,3 +26,35 @@ $(function() {
     });
     $("#amount").val($("#slider-horizontal").slider("value"));
 });
+
+$(window).resize(function() {
+    ancho_window = $(window).width();
+    alto_window = $(window).height();
+    checkResponsive();
+
+    if (collapsed == false) {
+        collapseHeader();
+    }
+    if (collapsed_mobile == false) {
+        collapseHeaderMobile();
+    }
+});
+
+
+
+/*
+collapseHeader();
+--------------------------------------------------
+Es la funcion para colapsar el header
+*/
+
+
+var collapseHeader = function() {
+    $("header nav").slideToggle(500);
+    collapsed = !collapsed;
+}
+
+var collapseHeaderMobile = function() {
+    $("#menuMobile").slideToggle(500);
+    collapsed_mobile = !collapsed_mobile;
+}
