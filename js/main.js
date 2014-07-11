cfd5781e3c1b5314a750d56f53a9c7aa38629359
juandinella <juan.dinella@gmail.com>
2014-07-11 19:14:33 -0300

    trust responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var collapsed = true;
-var collapsed_mobile = true;
+// var collapsed = true;
+// var collapsed_mobile = true;
 
 
 
@@ -26,35 +26,34 @@ $(function() {
     });
     $("#amount").val($("#slider-horizontal").slider("value"));
 });
+// $(window).resize(function() {
+//     ancho_window = $(window).width();
+//     alto_window = $(window).height();
+//     checkResponsive();
 
-$(window).resize(function() {
-    ancho_window = $(window).width();
-    alto_window = $(window).height();
-    checkResponsive();
-
-    if (collapsed == false) {
-        collapseHeader();
-    }
-    if (collapsed_mobile == false) {
-        collapseHeaderMobile();
-    }
-});
-
+//     if (collapsed == false) {
+//         collapseHeader();
+//     }
+//     if (collapsed_mobile == false) {
+//         collapseHeaderMobile();
+//     }
+// });
 
 
-/*
-collapseHeader();
---------------------------------------------------
-Es la funcion para colapsar el header
-*/
 
 
-var collapseHeader = function() {
-    $("header nav").slideToggle(500);
-    collapsed = !collapsed;
-}
+// collapseHeader();
+// --------------------------------------------------
+// Es la funcion para colapsar el header
 
-var collapseHeaderMobile = function() {
-    $("#menuMobile").slideToggle(500);
-    collapsed_mobile = !collapsed_mobile;
-}
+
+
+// var collapseHeader = function() {
+//     $("header nav").slideToggle(500);
+//     collapsed = !collapsed;
+// }
+
+// var collapseHeaderMobile = function() {
+//     $("#menuMobile").slideToggle(500);
+//     collapsed_mobile = !collapsed_mobile;
+// }
