6b92c9f59bdcb15abfdbcf28785105f755616668
juandinella <juan.dinella@gmail.com>
2014-07-25 09:07:02 -0300

    dns

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// var collapsed = true;
-// var collapsed_mobile = true;
-
-
-
-// $(document).ready(function() {
-//     $('.country').hover(function() {
-//         $(".othersCountries").slideDown("fast");
-//     }, function() {
-//         $(".othersCountries").slideUp("fast");
-//     });
-
-
-// });
 $(function() {
     $("#slider-horizontal").slider({
         orientation: "horizontal",
@@ -25,34 +11,3 @@ $(function() {
     });
     $("#amount").val($("#slider-horizontal").slider("value"));
 });
-// $(window).resize(function() {
-//     ancho_window = $(window).width();
-//     alto_window = $(window).height();
-//     checkResponsive();
-
-//     if (collapsed == false) {
-//         collapseHeader();
-//     }
-//     if (collapsed_mobile == false) {
-//         collapseHeaderMobile();
-//     }
-// });
-
-
-
-
-// collapseHeader();
-// --------------------------------------------------
-// Es la funcion para colapsar el header
-
-
-
-// var collapseHeader = function() {
-//     $("header nav").slideToggle(500);
-//     collapsed = !collapsed;
-// }
-
-// var collapseHeaderMobile = function() {
-//     $("#menuMobile").slideToggle(500);
-//     collapsed_mobile = !collapsed_mobile;
-// }
