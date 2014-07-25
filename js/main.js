33e8039c9ff2501ffe6ceeacdb8b0a92b35c1319
juandinella <juan.dinella@gmail.com>
2014-07-25 09:29:03 -0300

    js scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+/* ==========================================================================
+   Scroll
+   ========================================================================== */
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
 $(function() {
     $("#slider-horizontal").slider({
         orientation: "horizontal",
