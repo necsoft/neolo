45bbc194747746cd5d7d0bba98a70148cf1e78cb
Nec Necsoft <necsoftdesign@gmail.com>
2014-07-28 19:29:08 -0300

    Limpiado el codigo

--- js/housing.js
+++ js/housing.js
@@ -18,18 +18,18 @@ $(document).ready(function() {
         mb_value = 1;
 
     //Slider Horizontal
-    $("#slider-horizontal").slider({
-        orientation: "horizontal",
-        range: "min",
+    $('#slider-horizontal').slider({
+        orientation: 'horizontal',
+        range: 'min',
         min: 1,
         max: 50,
         value: 1,
         slide: function(event, ui) {
-            $("#amount").val(ui.value);
+            $('#amount').val(ui.value);
             checkPrice(this);
         }
     });
-    $("#amount").val($("#slider-horizontal").slider("value"));
+    $('#amount').val($('#slider-horizontal').slider('value'));
 
     //Toggle class de los features
     $features.click(function() {
@@ -49,61 +49,57 @@ $(document).ready(function() {
     // Chequear el precio
     function checkPrice(feature) {
 
-        if ($(feature).attr('id') === "feature_backup" && $(feature).hasClass("housingActive")) {
+        if ($(feature).attr('id') === 'feature_backup' && $(feature).hasClass('housingActive')) {
             price_value += 1080;
-        } else if ($(feature).attr('id') === "feature_backup") {
+        } else if ($(feature).attr('id') === 'feature_backup') {
             price_value -= 1080;
         }
 
-        if ($(feature).attr('id') === "feature_vpn" && $(feature).hasClass("housingActive")) {
+        if ($(feature).attr('id') === 'feature_vpn' && $(feature).hasClass('housingActive')) {
             price_value += 420;
-        } else if ($(feature).attr('id') === "feature_vpn") {
+        } else if ($(feature).attr('id') === 'feature_vpn') {
             price_value -= 420;
         }
 
 
-        if ($(feature).hasClass("trianguloder") && $(feature).parent().attr('id') === "feature_licencias") {
-            console.log("Agrego una licencia");
+        if ($(feature).hasClass('trianguloder') && $(feature).parent().attr('id') === 'feature_licencias') {
             if (licencias_value >= 0) {
                 licencias_value++;
                 price_value += 240;
-                $(".licencias").text(licencias_value);
-                $(".licencias").parent().addClass("housingActive");
+                $('.licencias').text(licencias_value);
+                $('.licencias').parent().addClass('housingActive');
             }
-        } else if ($(feature).hasClass("trianguloizq") && $(feature).parent().attr('id') === "feature_licencias") {
-            console.log("Resto una licencia");
+        } else if ($(feature).hasClass('trianguloizq') && $(feature).parent().attr('id') === 'feature_licencias') {
             if (licencias_value > 0) {
                 licencias_value--;
                 price_value -= 240;
-                $(".licencias").text(licencias_value);
+                $('.licencias').text(licencias_value);
                 if (licencias_value === 0) {
-                    $(".licencias").parent().removeClass("housingActive");
+                    $('.licencias').parent().removeClass('housingActive');
                 }
             }
         }
 
-        if ($(feature).hasClass("trianguloder") && $(feature).parent().attr('id') === "feature_ip") {
-            console.log("Agrego una ip");
+        if ($(feature).hasClass('trianguloder') && $(feature).parent().attr('id') === 'feature_ip') {
             if (ips_value >= 0) {
                 ips_value++;
                 price_value += 24;
-                $(".adicionalip").text(ips_value);
-                $(".adicionalip").parent().addClass("housingActive");
+                $('.adicionalip').text(ips_value);
+                $('.adicionalip').parent().addClass('housingActive');
             }
-        } else if ($(feature).hasClass("trianguloizq") && $(feature).parent().attr('id') === "feature_ip") {
-            console.log("Resto una ip");
+        } else if ($(feature).hasClass('trianguloizq') && $(feature).parent().attr('id') === 'feature_ip') {
             if (ips_value > 0) {
                 ips_value--;
                 price_value -= 24;
-                $(".adicionalip").text(ips_value);
+                $('.adicionalip').text(ips_value);
                 if (ips_value === 0) {
-                    $(".adicionalip").parent().removeClass("housingActive");
+                    $('.adicionalip').parent().removeClass('housingActive');
                 }
 
             }
         }
 
-        mb_value = $("#amount").val();
+        mb_value = $('#amount').val();
         if (mb_value > 1 && mb_value < 6) {
             $price.text(price_value + ((mb_value - 1) * 550));
         } else if (mb_value >= 6 && mb_value < 11) {
@@ -114,7 +110,4 @@ $(document).ready(function() {
             $price.text(price_value);
         }
     }
-
-
-    console.log(price_value);
 });
